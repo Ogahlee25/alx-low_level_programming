b59bf12ef3a45a24d463da8c5f368533bf6425e4
ogahlee25 <ogalipeace905@gmail.com>
2023-04-27 09:56:38 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,17 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-* main - Test function for positive or negative
-* Return: 0
-*/
-int main(void)
+ * positive_or_negative - checks if the given number i is +, - or 0
+ * @i: number to be checked
+ * Return: Nothing on success
+ */
+void positive_or_negative(int i)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 }
